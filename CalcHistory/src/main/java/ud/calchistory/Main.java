b05a891d63b8e719fca8db47376bf35930baa104
02NIN20 <lycoronelp@udistrital.edu.co>
2025-06-04 14:20:03 -0500

    a

--- CalcHistory/src/main/java/ud/calchistory/Main.java
+++ CalcHistory/src/main/java/ud/calchistory/Main.java
@@ -3,7 +3,7 @@ package ud.calchistory;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] lol) {
         Calculadora calculadora = new Calculadora();
         Caretaker caretaker = new Caretaker();
         Scanner scanner = new Scanner(System.in);
